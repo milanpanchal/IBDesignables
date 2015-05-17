16cb4c3f8595eb7ac769c5ed2abfc90aeb2c6875
Milan Panchal <sam07it22@gmail.com>
2015-05-17 22:50:42 +0530

    Initial Commit

---
+++ IBDesignables/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IBDesignables
+//
+//  Created by MilanPanchal on 17/05/15.
+//  Copyright (c) 2015 Pantech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
